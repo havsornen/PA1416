9299183f1b38dc37c1c764959d6749a28e7b7450
bjorn <bjoand12@gmail.com>
2019-04-15 15:13:49 +0100

    landingpage added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'streaming', loadChildren: './streaming/streaming.module#StreamingPageModule' },
+  { path: 'landingpage', loadChildren: './landingpage/landingpage.module#LandingpagePageModule' },
 ];
 
 @NgModule({

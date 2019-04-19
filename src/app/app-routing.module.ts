286302ccfa871a867309649588a5eaab4634670f
Unsinkablesam <maraphostv@gmail.com>
2019-04-19 11:13:28 +0200

    adding streaming serivce client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'streaming', loadChildren: './streaming/streaming.module#StreamingPageModule' },
   { path: 'landingpage', loadChildren: './landingpage/landingpage.module#LandingpagePageModule' },
 ];
+
+
 
 @NgModule({
   imports: [

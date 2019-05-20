252b7d54f02aca15e4ffa9e20cf84e6e6407715c
Cabbex <erik.casper.bjork@gmail.com>
2019-05-20 17:21:48 +0200

    User can now register on the site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import {AuthGateService} from './auth-gate.service';
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'landingpage', loadChildren: './landingpage/landingpage.module#LandingpagePageModule', canActivate: [AuthGateService] },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'activity', loadChildren: './activity/activity.module#ActivityPageModule' },
+  { path: 'register', loadChildren: './create-profile/create-profile.module#CreateProfilePageModule'}
 ];
 
 @NgModule({
--- src/app/create-profile/create-profile.page.ts
+++ src/app/create-profile/create-profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-profile',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateProfilePage implements OnInit {
 
-  constructor() { }
+  email_input;
+  firstname_input;
+  lastname_input;
+  password_input;
+  headers = {
+    'headers': new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  private register() {
+    const user_data = {
+      'email' : this.email_input,
+      'pwd' : this.password_input,
+      'fName' : this.firstname_input,
+      'lName' : this.lastname_input,
+      'telNumber' : '+46'
+    };
+
+    const URL = 'http://localhost:8080/SE_RESTApi_war_exploded/user/create';
+
+    this.http.post(URL, user_data, this.headers)
+        .subscribe((response) => {
+          console.log('Success: ' + response['usr_email']);
+          this.router.navigate(['login']);
+        }, error => {
+          console.log('Error: ' + error.error['response']);
+        });
   }
 
 }

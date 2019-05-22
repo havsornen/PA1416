0ec222e4522b52a3117dbb2b673e20c74829ada7
Björn Andersson <bjoand12@gmail.com>
2019-05-22 16:50:05 +0200

    Feed page update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'streaming', loadChildren: './streaming/streaming.module#StreamingPageModule' },
-  { path: 'landingpage', loadChildren: './landingpage/landingpage.module#LandingpagePageModule', canActivate: [AuthGateService] },
+  { path: 'landingpage', loadChildren: './landingpage/landingpage.module#LandingpagePageModule', /*canActivate: [AuthGateService]*/ },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'activity', loadChildren: './activity/activity.module#ActivityPageModule' },
-  { path: 'register', loadChildren: './create-profile/create-profile.module#CreateProfilePageModule'}
+  { path: 'register', loadChildren: './create-profile/create-profile.module#CreateProfilePageModule'},
+  { path: 'create', loadChildren: './create-activity/create-activity.module#CreateActivityPageModule'},  { path: 'feed', loadChildren: './feed/feed.module#FeedPageModule' }
+
 ];
 
 @NgModule({

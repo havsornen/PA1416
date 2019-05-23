7c8afefe57d2ff0a3841c5131253cdbfce265c51
Cabbex <erik.casper.bjork@gmail.com>
2019-05-23 23:16:33 +0200

    Added connectivity to activity creation and feed and fixed some past feed issues.

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedPage implements OnInit {
 
-  constructor() { }
+  activites;
+
+  constructor(private http: HttpClient, private us: UserServiceService) { }
 
   ngOnInit() {
+    this.load_activites();
+  }
+
+  private load_activites() {
+    const userData = Object.values(this.us.getUser());
+    const userID = userData[1];
+    const URL = 'http://localhost:8080/SE_RESTApi_war_exploded/activity/activity_feed/' + userID;
+
+    this.http.get(URL)
+        .subscribe((response) => {
+          console.log(response);
+          this.activites = response;
+        }, error => {
+          console.log(error);
+        });
   }
 
 }
